b916a15abdb27e59ac1b0c3d749de1ba8e172891
August-IE <Aeromosele9@gmail.com>
2023-06-15 13:03:31 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,1 @@
-#Include <stdio.h>
-/**
- * main - A program which prints puts on a line
- * Return : 0 (Success)
- */
-int main(void)
-{
-puts ("/"Programming is like building a multilingual puzzle");
-return (0)
-}
+
