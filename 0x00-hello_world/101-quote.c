fed1f5eeb90056ade37202bd4e8eb54d8df5fa85
August-IE <Aeromosele9@gmail.com>
2023-06-15 14:38:38 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 include <stdio.h>
 include <unistd.h>
+
 /**
- * main - A C program that prints exactly a sentence followed by a new line, to the standard error.
- * Return 1 (Success)
+ * main - A c program that prints exactly a sentence followed by a new line, to the standard error.
+ * Return: 1 (Success)
  */
 int main(void)
 {
