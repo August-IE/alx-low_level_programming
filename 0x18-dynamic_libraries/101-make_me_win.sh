b7a146ca69ba2f01c676ee203542561ec7b5a7fe
August-IE <Aeromosele9@gmail.com>
2023-09-18 15:54:26 -0700

    advanced task wahala

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/August-IE/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so -P /tmp/
-export LD_PRELOAD=/tmp/libdynamic.so
+wget https://raw.githubusercontent.com/alx-tools/0x18.c/master/gm -O /tmp/gm
+export LD_PRELOAD=/tmp/gm
