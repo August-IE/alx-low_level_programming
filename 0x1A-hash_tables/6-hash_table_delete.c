b832f82e3aa4e048dc68996f89b145fbc8e7a3a4
August-IE <Aeromosele9@gmail.com>
2023-10-06 06:44:03 -0700

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,9 +18,10 @@ void hash_table_delete(hash_table_t *ht)
 		hash_node_t *tmp = node->next;
 
 		free(node->key);
-		free(node);
-		node = tmp;
 		free(node->value);
+		free(node);
+
+		node = tmp;
 	}
 	}
 
